a67b245efaa4f2789b6115b9a0c4c609f0b244a3
Jan Kaifer <jan.kaifer@cognifence.ai>
2026-02-06 21:48:02 +0100

    Revert "Test rollback by denying all SSH logins"
    This reverts commit c6cf1b6b66409e38a1614403dee8caa24e9eb8a0.

--- machines/frame1/default.nix
+++ machines/frame1/default.nix
@@ -45,7 +45,7 @@ in
     settings = {
       PermitRootLogin = "no"; # Disable direct root SSH login
       PasswordAuthentication = false; # Enforce SSH key-only auth
-      AllowUsers = [ "disabled-for-rollback-test" ]; # Intentionally deny all real users
+      AllowUsers = [ "jankaifer" ]; # Restrict SSH access to main operator
     };
   };
 
